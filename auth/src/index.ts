3a588fa05ce7519d8d13df07b98f365a8503a189
Vinh Vu <vinhhv@uci.edu>
2024-07-19 11:47:56 -0700

    Add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import { json } from "body-parser";
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/sign-in";
+import { signoutRouter } from "./routes/sign-out";
+import { signupRouter } from "./routes/sign-up";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!");
