06d09a9ebbdaa70829019c92e6289b659a4eaedf
Vinh Vu <vinhhv@uci.edu>
2024-07-20 12:27:26 -0700

    Add custom error abstract class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,11 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super();
+    super("Invalid request parameters");
 
     // Only because we are extending a built-in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
@@ -15,6 +16,7 @@ export class RequestValidationError extends Error {
       if (error.type === "field") {
         return { message: error.msg, field: error.path };
       }
+      return { message: error.msg };
     });
   }
 }
